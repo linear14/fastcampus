389321f47c4c2967330c8f52ee32291e132367f4
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-05-12 19:33:41 +0900

    Add: project4 레이아웃 작업

--- project04/app/src/main/java/com/ldh/project04/MainActivity.kt
+++ project04/app/src/main/java/com/ldh/project04/MainActivity.kt
@@ -2,10 +2,31 @@ package com.ldh.project04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View) {
+
+    }
+
+    fun signButtonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
+
+    }
+
+    fun recordButtonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
+
     }
 }
