8b250c030f908c9cf19cd88f106cf220f49e3413
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-05-18 08:35:09 +0900

    Add: project5 권한 작업
    - checkSelfPermission
    - shouldShowRequestPermissionRationale
    - requestPermissions

--- project05/app/src/main/java/com/ldh/project05/MainActivity.kt
+++ project05/app/src/main/java/com/ldh/project05/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.ldh.project05
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.ldh.project05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAddButton()
+        initStartFrameModeButton()
+    }
+
+    private fun initAddButton() {
+        binding.apply {
+            btnAddPhoto.setOnClickListener {
+                when {
+                    // 권한이 이미 승인되어 있으면
+                    ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED -> {
+
+                    }
+                    // 권한 필요성에 대한 부연 설명이 필요한지 판단, 그렇다면 팝업
+                    // (처음 요청할때는 false, 한 번 거절당하고 부터는 true가 들어오는 것 같음. 경험으로는!)
+                    shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                        showPermissionContextPopup()
+                    }
+                    // 권한 요청
+                    else -> {
+                        requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initStartFrameModeButton() {
+
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자에 앱에서 사진을 불러오기 위해 저장소 접근 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
     }
 }
