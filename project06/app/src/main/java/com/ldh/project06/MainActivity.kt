51ea8349599028c47a77ae8d5bccfb0bc7f28ec6
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-05-20 11:50:11 +0900

    Add: project6 SoundPool 음향효과 + 디자인 (완성)

--- project06/app/src/main/java/com/ldh/project06/MainActivity.kt
+++ project06/app/src/main/java/com/ldh/project06/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ldh.project06
 
 import android.annotation.SuppressLint
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -22,13 +23,18 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekbar)
     }
 
-    var countDownTimer: CountDownTimer? = null
+    private val soundPool = SoundPool.Builder().build()
+
+    private var countDownTimer: CountDownTimer? = null
+    private var tickSound: Int? = null
+    private var bellSound: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+        initSounds()
     }
 
     private fun bindViews() {
@@ -42,38 +48,68 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                countDownTimer?.cancel()
-                countDownTimer = null
+                stopCountDown()
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 if(seekBar != null) {
-                    createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    if(seekBar.progress == 0) {
+                        stopCountDown()
+                    } else {
+                        startCountDown()
+                    }
                 }
             }
 
         })
     }
 
-    private fun createCountDownTimer(initTimeInMillis: Long) {
-        countDownTimer = object: CountDownTimer(initTimeInMillis, 1000) {
+    private fun initSounds() {
+        tickSound = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSound = soundPool.load(this, R.raw.timer_bell, 1)
+    }
+
+    private fun createCountDownTimer(initTimeInMillis: Long)
+        = object: CountDownTimer(initTimeInMillis, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 updateRemain(millisUntilFinished)
                 updateSeekBar(millisUntilFinished)
             }
 
             override fun onFinish() {
-                updateRemain(0)
-                updateSeekBar(0)
+                completeCountDown()
             }
-
         }.start()
+
+
+    private fun startCountDown() {
+        countDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+
+        tickSound?.let {
+            soundPool.play(it, 1f, 1f, 0, -1, 1f)
+        }
+    }
+
+    private fun stopCountDown() {
+        countDownTimer?.cancel()
+        countDownTimer = null
+        soundPool.autoPause()
+    }
+
+    private fun completeCountDown() {
+        updateRemain(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSound?.let {
+            soundPool.play(it, 1f, 1f, 0, 0, 1f)
+        }
     }
 
     private fun updateRemain(remainedMillis: Long) {
         val remainSeconds = remainedMillis / 1000
 
-        tvRemainedMinute.text = "%02d".format(remainSeconds / 60)
+        tvRemainedMinute.text = "%02d`".format(remainSeconds / 60)
         tvRemainedSecond.text = "%02d".format(remainSeconds % 60)
     }
 
