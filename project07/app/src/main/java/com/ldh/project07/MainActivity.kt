e68802ce7e6617100ed19bf6743e104e8288308e
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-06-14 16:53:21 +0900

    Add: project6 권한 설정 및 MediaRecorder, MediaPlayer 사용

--- project07/app/src/main/java/com/ldh/project07/MainActivity.kt
+++ project07/app/src/main/java/com/ldh/project07/MainActivity.kt
@@ -1,24 +1,136 @@
 package com.ldh.project07
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_RECORD_AUDIO_PERMISSION = 201
+    }
 
     private val recordButton: RecordButton by lazy {
         findViewById(R.id.btn_record)
     }
 
+    private val resetButton: Button by lazy{
+        findViewById(R.id.btn_reset)
+    }
+
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
     private var state = State.BEFORE_RECORDING
+        set(value) {
+            field = value
+            resetButton.isEnabled = (value == State.AFTER_RECORDING) || (value == State.ON_PLAYING)
+            recordButton.updateIconWithState(field)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
+
+        initVariables()
         initViews()
+        bindViews()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initVariables() {
+        state = State.BEFORE_RECORDING
     }
 
     private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+        recordButton.setOnClickListener {
+            when(state) {
+                State.BEFORE_RECORDING -> {
+                    startRecording()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+
+        resetButton.setOnClickListener {
+            stopPlaying()
+            state = State.BEFORE_RECORDING
+        }
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+        recorder?.start()
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release() // 메모리 해제
+        }
+        recorder = null
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            setDataSource(recordingFilePath)
+            prepare()   // prepareAsync도 존재
+        }
+        player?.start()
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
     }
 }
